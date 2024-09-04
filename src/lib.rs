7a527f6de6508ea6821a8829b8cd88625766748b
IFFranciscoME <if.francisco.me@gmail.com>
2024-09-04 10:32:11 -0600

    Changes for docs.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,30 @@
+//! This crate provides a function to calculate the Jaccard similarity between two queries.
+
 use regex::Regex;
 use std::collections::HashSet;
+
+/// Itemizes a query string based on a given pattern using regular expressions.
+///
+/// # Arguments
+///
+/// * `query` - The input query string to be itemized.
+/// * `pattern` - The regular expression pattern used for itemization.
+///
+/// # Returns
+///
+/// A `HashSet` containing the itemized tokens from the query, converted to uppercase.
+///
+/// # Examples
+///
+/// ```rust
+/// use std::collections::HashSet;
+/// use jaccard_similarity::itemize_query;
+///
+/// let query = "SELECT * FROM users WHERE age > 30";
+/// let pattern = r"\b(SELECT|WHERE|FROM|AND|OR)\B|\w+";
+/// let tokens: HashSet<_> = itemize_query(query, pattern);
+/// assert_eq!(tokens, ["SELECT", "WHERE", "FROM", "users", "age"].iter().cloned().collect());
+/// ```
 
 fn itemize_query(query: &str, pattern: &str) -> HashSet<String> {
     let re = Regex::new(pattern).unwrap();
@@ -7,6 +32,32 @@ fn itemize_query(query: &str, pattern: &str) -> HashSet<String> {
         .map(|matched| matched.as_str().to_uppercase())
         .collect()
 }
+
+/// Calculates the Jaccard similarity between two queries.
+///
+/// The Jaccard similarity is a measure of similarity between two sets, defined as the size of the intersection divided by the size of the union of the sets.
+///
+/// # Arguments
+///
+/// * `query1` - The first query string.
+/// * `query2` - The second query string.
+/// * `itemizer_pattern` - The regular expression pattern used for itemizing the queries.
+///
+/// # Returns
+///
+/// The Jaccard similarity between the two queries, as a floating-point value between 0.0 and 1.0.
+///
+/// # Examples
+///
+/// ```rust
+/// use jaccard_similarity::jaccard_similarity;
+///
+/// let query1 = "SELECT * FROM users WHERE age > 30";
+/// let query2 = "SELECT * FROM users WHERE age > 31";
+/// let pattern = r"\b(SELECT|WHERE|FROM|AND|OR)\B|\w+";
+/// let similarity = jaccard_similarity(query1, query2, pattern);
+/// assert!(similarity > 0.7 && similarity < 0.8);
+/// ```
 
 pub fn jaccard_similarity(query1: &str, query2: &str, itemizer_pattern: &str) -> f64 {
     let tokens1: HashSet<_> = itemize_query(query1, itemizer_pattern);
